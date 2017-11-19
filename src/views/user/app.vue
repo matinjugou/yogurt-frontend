dfd061c942aeac80b475b8358fa40ed1e5b3b9f0
Zhao <zhaoshangming1997@gmail.com>
2017-11-19 13:16:15 +0800

    added chatting ui

--- src/views/user/app.vue
+++ src/views/user/app.vue
@@ -1,46 +1,57 @@
 <template>
   <div id="app">
-    <div class="layout" :class="{'layout-hide-text': spanLeft < 5}">
+    <div class="layout">
       <Row type="flex">
-        <Col :span="spanLeft" class="layout-menu-left">
+        <Col span="5" class="layout-menu-left">
         <Menu active-name="1" theme="dark" width="auto">
         </Menu>
         </Col>
-        <Col :span="spanRight">
-        <div class="layout-header">
-          <Button type="text" @click="toggleClick">
-            <Icon type="navicon" size="32"></Icon>
-          </Button>
-          <Avatar class="inline-middle" shape="square" icon="person"/>
-          <span class="inline-middle">xxx公司</span>
-          <span style="float: right;margin-top: 12px;margin-right: 21px">人工客服</span>
+        <Col span="19">
+        <div class="chat-header">
+          <div class="chat-title chat-title-company-name">
+            XXX公司
+          </div>
+          <div class="chat-title chat-title-staff-type">
+            售后
+          </div>
+          <!--<Button type="text" @click="toggleClick">-->
+            <!--<Icon type="navicon" size="32"></Icon>-->
+          <!--</Button>-->
+          <!--<Avatar class="inline-middle" shape="square" icon="person"/>-->
+          <!--<span class="inline-middle">xxx公司</span>-->
+          <!--<span style="float: right;margin-top: 12px;margin-right: 21px">人工客服</span>-->
         </div>
-        <div class="layout-content-msg">
-          <div class="layout-content-msg-main">messages</div>
+        <!--<div class="layout-content-msg">-->
+          <!--<div class="layout-content-msg-main">messages</div>-->
+        <!--</div>-->
+        <div class="chat-content">
+          <ul>
+            <li v-for="(singleRecord, index) in currentChatRecord" :key="singleRecord.id">
+              <p class="chat-msg-time">
+                <span>{{ singleRecord.time }}</span>
+              </p>
+              <div class="chat-msg-body" :class="[{'from-me': singleRecord.from.startsWith('user')}]">
+                <div class="avatar chat-single-record">
+                  <Avatar shape="square" icon="person"/>
+                </div>
+                <div class="content chat-single-record" :class="[singleRecord.type]">{{ singleRecord.msg }}</div>
+              </div>
+            </li>
+          </ul>
         </div>
-        <div class="layout-content-input">
-          <div class="layout-content-input-main">
-            <Button>
-              文件
-              <Icon type="document"></Icon>
-            </Button>
-            <Button>
-              表情
-              <Icon type="android-happy"></Icon>
-            </Button>
-            <Button>
-              历史消息
-            </Button>
-            <Button>
-              截图
-              <Icon type="monitor"></Icon>
-            </Button>
-            <Button style="float: right">发送</Button>
-            <Input type="textarea" style="margin-top: 5px" :rows="6" placeholder="在此输入消息"></Input>
+        <div class="chat-input-actions">
+          <div class="chat-input-media">
+            <Button type="ghost" icon="document">文件</Button>
+            <Button type="ghost" icon="happy">表情</Button>
+            <Button type="ghost">历史消息</Button>
+            <Button type="ghost" icon="monitor">截图</Button>
+          </div>
+          <div class="chat-input-send">
+            <Button type="success" icon="paper-airplane" @click="sendMessage">发送</Button>
           </div>
         </div>
-        <div class="layout-copy">
-          2017 &copy; Yogurt
+        <div class="chat-input">
+          <Input v-model="inputText" type="textarea" :rows="8" placeholder="在这里输入信息，按Ctrl+Enter发送"></Input>
         </div>
         </Col>
       </Row>
@@ -63,31 +74,31 @@
     border-radius: 4px;
     overflow: hidden;
   }
-  .layout-content-msg{
-    height: calc(100vh - 60px - 30px - 30px - 200px);
-    margin: 15px;
-    margin-bottom: 2px;
-    overflow: hidden;
-    background: #fff;
-    border-radius: 4px;
-    box-shadow: 0 0 1px rgba(0,0,0,.1);
-  }
-  .layout-content-msg-main{
-    padding: 10px;
-  }
-  .layout-content-input{
-    height: 200px;
-    overflow: hidden;
-    margin: 15px;
-    margin-top: 0;
-    background: #fff;
-    border-radius: 4px;
-    box-shadow: 0 1px 1px rgba(0,0,0,.1);
-  }
-  .layout-content-input-main{
-    padding: 10px;
-    max-height: 200px;
-  }
+  /*.layout-content-msg{*/
+    /*height: calc(100vh - 60px - 30px - 30px - 200px);*/
+    /*margin: 15px;*/
+    /*margin-bottom: 2px;*/
+    /*overflow: hidden;*/
+    /*background: #fff;*/
+    /*border-radius: 4px;*/
+    /*box-shadow: 0 0 1px rgba(0,0,0,.1);*/
+  /*}*/
+  /*.layout-content-msg-main{*/
+    /*padding: 10px;*/
+  /*}*/
+  /*.layout-content-input{*/
+    /*height: 200px;*/
+    /*overflow: hidden;*/
+    /*margin: 15px;*/
+    /*margin-top: 0;*/
+    /*background: #fff;*/
+    /*border-radius: 4px;*/
+    /*box-shadow: 0 1px 1px rgba(0,0,0,.1);*/
+  /*}*/
+  /*.layout-content-input-main{*/
+    /*padding: 10px;*/
+    /*max-height: 200px;*/
+  /*}*/
   .layout-copy{
     text-align: center;
     /*padding: 10px 0 20px;*/
@@ -97,16 +108,93 @@
   .layout-menu-left{
     background: #464c5b;
   }
-  .layout-header{
-    font-size: 18px;
+  .chat-header{
+    /*font-size: 18px;*/
+    /*height: 60px;*/
+    /*background: #fff;*/
+    /*box-shadow: 0 1px 1px rgba(0,0,0,.1);*/
     height: 60px;
-    background: #fff;
+    background: #FFFFFF;
+    box-shadow: 0 1px 1px rgba(0,0,0,.1);
+    padding: 0px 10px 0px 40px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+  .chat-title{
+    color: #495060;
+    font-size: 20px;
+    font-weight: Bold;
+  }
+  .chat-content{
+    height: calc(100vh - 290px);
+    padding: 10px 15px;
+    overflow: auto;
+  }
+  .chat-msg-time {
+    margin: 7px 0;
+    text-align: center;
+  }
+  .chat-msg-time > span {
+    display: inline-block;
+    padding: 0 18px;
+    font-size: 12px;
+    color: #fff;
+    border-radius: 2px;
+    background-color: #dcdcdc;
+  }
+  .chat-single-record {
+    display: inline-flex;
+    vertical-align: text-top;
+    margin-right: 10px;
+  }
+  .chat-msg-body > .content {
+    display: inline-block;
+    position: relative;
+    padding: 0 10px;
+    max-width: calc(100vw - 40px);
+    min-height: 30px;
+    line-height: 2.5;
+    font-size: 16px;
+    text-align: left;
+    word-break: break-all;
+    background-color: #FFF;
+    border-radius: 4px;
     box-shadow: 0 1px 1px rgba(0,0,0,.1);
   }
-  .inline-middle{
-    display: inline-block;
-    margin: 3px;
-    vertical-align: middle;
+  .chat-msg-body > .content::before {
+    content: " ";
+    position: absolute;
+    top: 9px;
+    right: 100%;
+    border: 6px solid transparent;
+    border-right-color: #ffffff;
+  }
+  .from-me {
+    text-align: right;
+  }
+  .from-me > .avatar {
+    float: right;
+    margin: 0 0 0 10px;
+  }
+  .from-me > .content {
+    color: #ffffff;
+    background-color: #2d8cf0;
+  }
+  .from-me > .content::before {
+    right: inherit;
+    left: 100%;
+    border-right-color: transparent;
+    border-left-color: #2d8cf0;
+  }
+  .chat-input-actions{
+    height: 50px;
+    background: #FFFFFF;
+    box-shadow: 0 1px 1px rgba(0,0,0,.1);
+    padding: 0px 30px 0px 10px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
   }
   .layout-ceiling-msg a{
     color: #9ba7b5;
@@ -119,28 +207,65 @@
   }
 </style>
 <script>
-  export default {
-    data () {
-      return {
-        spanLeft: 5,
-        spanRight: 19
-      }
-    },
-    computed: {
-      iconSize () {
-        return this.spanLeft === 5 ? 14 : 24
-      }
-    },
-    methods: {
-      toggleClick () {
-        if (this.spanLeft === 5) {
-          this.spanLeft = 2
-          this.spanRight = 22
-        } else {
-          this.spanLeft = 5
-          this.spanRight = 19
+//  export default {
+//    data () {
+//      return {
+//        spanLeft: 5,
+//        spanRight: 19
+//      }
+//    },
+//    computed: {
+//      iconSize () {
+//        return this.spanLeft === 5 ? 14 : 24
+//      }
+//    },
+//    methods: {
+//      toggleClick () {
+//        if (this.spanLeft === 5) {
+//          this.spanLeft = 2
+//          this.spanRight = 22
+//        } else {
+//          this.spanLeft = 5
+//          this.spanRight = 19
+//        }
+//      }
+//    }
+//  }
+export default {
+  name: 'UserChat',
+  data () {
+    return {
+      userId: '1',
+      staffId: '2',
+      contentList: [
+        {
+          id: '1',
+          msg: 'Hello, I\'m staff1.',
+          from: 'staff_2',
+          to: 'user_1',
+          type: 'text',
+          time: '2017-11-19 15:39:14'
+        },
+        {
+          id: '2',
+          msg: 'Hello, I\'m user.',
+          from: 'user_1',
+          to: 'staff_2',
+          type: 'text',
+          time: '2017-11-19 15:39:15'
         }
-      }
+      ]
+    }
+  },
+  computed: {
+    currentChatRecord () {
+      return this.contentList
+    }
+  },
+  methods: {
+    sendMessage () {
+      // TODO: send message
     }
   }
+}
 </script>
